e7522e37bccd7cfcfe9ef4a0df4dcca8f37a73d5
ofernandezOaro <ofernandez@oaro.net>
2025-09-28 01:07:49 +0200

    Delete api from url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,13 +1,13 @@
 // Configuración de la API
 export const API_CONFIG = {
-    BASE_URL: process.env.NEXT_PUBLIC_API_URL || "",
-    ENDPOINTS: {
-        LOGIN: "/api/login",
-        REGISTER: "/api/register",
-        USER: "/api/user",
-        LOGOUT: "/api/logout",
-        SESSIONS: "/api/sessions",
-        RESERVE_SESSION: "/api/reserve-session",
-        CANCEL_RESERVATION: "/api/cancel-reservation",
-    },
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL || "",
+  ENDPOINTS: {
+    LOGIN: "/login",
+    REGISTER: "/register",
+    USER: "/user",
+    LOGOUT: "/logout",
+    SESSIONS: "/sessions",
+    RESERVE_SESSION: "/reserve-session",
+    CANCEL_RESERVATION: "/cancel-reservation",
+  },
 };
